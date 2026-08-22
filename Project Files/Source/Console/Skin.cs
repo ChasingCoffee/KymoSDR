0216c12b620b4f6a1ee4403d5d534e030b1a57bf
nubbyless <nub@nubbyswasteland.com>
2026-08-22 17:45:47 -0400

    3D Pan button: alias to chkNoiseGate skin image set like TXVST/RXVST

--- Project Files/Source/Console/Skin.cs
+++ Project Files/Source/Console/Skin.cs
@@ -1000,7 +1000,7 @@ namespace Thetis
         private static void SetupCheckBoxImages(CheckBox ctrl)
         {
             string imgName = ctrl.Name;
-            if (imgName == "chkTXVST" || imgName == "chkRXVST")
+            if (imgName == "chkTXVST" || imgName == "chkRXVST" || imgName == "btnDisplay3DPan")
                 imgName = "chkNoiseGate";
 
             string skey = "";
